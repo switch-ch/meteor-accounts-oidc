f78a3801f67ad9766f16b8924409d4a8ab2d6b8c
Valery Tschopp <valery.tschopp@switch.ch>
2017-01-19 09:23:19 +0100

    v1.0.5
    idTokenWhitelsitFields feature

--- packages/switch:accounts-oidc/package.js
+++ packages/switch:accounts-oidc/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "OpenID Connect (OIDC) for Meteor accounts",
-  version: "1.0.4",
+  version: "1.0.5",
   name: "switch:accounts-oidc",
   git: "https://github.com/switch-ch/meteor-accounts-oidc.git",
 
