3404d90c9595f106ac90e4847043e4fa32dcf004
Valery Tschopp <valery.tschopp@switch.ch>
2017-10-10 17:35:37 +0200

    v1.0.5: Support login URLs that already contain parameters

--- packages/switch.oidc/package.js
+++ packages/switch.oidc/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "OpenID Connect (OIDC) flow for Meteor",
-  version: "1.0.4",
+  version: "1.0.5",
   name: "switch:oidc",
   git: "https://github.com/switch-ch/meteor-accounts-oidc.git",
 });
