6b456b35a775febf6f6aa375507a346018fdf7b8
Valery Tschopp <valery.tschopp@switch.ch>
2019-02-27 17:06:59 +0100

    api.use('underscore@1.0.0', ['server', 'client']);

--- packages/switch_oidc/package.js
+++ packages/switch_oidc/package.js
@@ -9,8 +9,7 @@ Package.onUse(function(api) {
   api.use('oauth2@1.1.0', ['client', 'server']);
   api.use('oauth@1.1.0', ['client', 'server']);
   api.use('http@1.1.0', 'server');
-  api.use('underscore', 'server');
-  api.use('underscore@1.0.0', 'client');
+  api.use('underscore@1.0.0', ['server', 'client']);
   api.use('templating@1.1.0', 'client');
   api.use('random@1.0.0', 'client');
   api.use('service-configuration@1.0.0', ['client', 'server']);
