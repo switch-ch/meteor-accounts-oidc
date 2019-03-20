eb338e8a1d53161541e3657baeb3771df26d038b
Valery Tschopp <valery.tschopp@switch.ch>
2019-03-20 14:47:48 +0100

    version bump

--- packages/switch_accounts-oidc/package.js
+++ packages/switch_accounts-oidc/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "OpenID Connect (OIDC) for Meteor accounts",
-  version: "1.0.6",
+  version: "1.0.7",
   name: "switch:accounts-oidc",
   git: "https://github.com/switch-ch/meteor-accounts-oidc.git",
 
@@ -12,7 +12,8 @@ Package.onUse(function(api) {
   // Export Accounts (etc) to packages using this one.
   api.imply('accounts-base', ['client', 'server']);
   api.use('accounts-oauth@1.1.0', ['client', 'server']);
-  api.use('switch:oidc@1.0.0', ['client', 'server']);
+
+  api.use('switch:oidc@1.0.7', ['client', 'server']);
 
   api.addFiles('oidc_login_button.css', 'client');
 
